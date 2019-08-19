67345d663b925a81a625ac5c6dec3a2a2774a6a4
yourentutu <2290322442@qq.com>
2019-08-19 22:50:44 +0800

    leetcode

--- src/main/java/com.hc/leetcode/ThreeSum.java
+++ src/main/java/com.hc/leetcode/ThreeSum.java
@@ -8,7 +8,6 @@ public class ThreeSum {
     public List<List<Integer>> threeSum(int[] nums) {
         Arrays.sort(nums);
         List<List<Integer>> result = new ArrayList<>();
-
         for (int i = 0; i <nums.length-2 ; i++) {
             if (i>0&&nums[i]==nums[i-1]){
                 continue;
